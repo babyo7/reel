899e721ee84ac89f10ddc4ef6a006da659bdb4cc
babyo77 <onlybabyyo7@gmail.com>
2024-04-14 01:25:27 +0530

    new

--- fileStream.js
+++ fileStream.js
@@ -3,26 +3,13 @@ const client = new SoundCloud.Client();
 const fs = require("fs");
 
 module.exports = function fileStream(res, url, File) {
-  client.getSongInfo(url).then(async (song) => {
-    const isfile = `music/${song.id}.mp3`;
-    if (fs.existsSync(isfile)) {
-      const stream = fs.createReadStream(isfile);
-      const data = fs.readFileSync(isfile);
-      res.setHeader("content-type", "audio/mpeg");
-      res.setHeader("Accept-Ranges", "bytes");
-      res.setHeader("content-length", data.length);
-      res.setHeader(
-        "Content-Disposition",
-        `attachment; filename="${File}.mp3"`
-      );
-      return stream.pipe(res);
-    } else {
-      const stream = await song.downloadProgressive();
-      const writer = stream.pipe(fs.createWriteStream(isfile));
-      writer.on("finish", () => {
+  client
+    .getSongInfo(url)
+    .then(async (song) => {
+      const isfile = `music/${song.id}.mp3`;
+      if (fs.existsSync(isfile)) {
         const stream = fs.createReadStream(isfile);
         const data = fs.readFileSync(isfile);
-
         res.setHeader("content-type", "audio/mpeg");
         res.setHeader("Accept-Ranges", "bytes");
         res.setHeader("content-length", data.length);
@@ -30,8 +17,26 @@ module.exports = function fileStream(res, url, File) {
           "Content-Disposition",
           `attachment; filename="${File}.mp3"`
         );
-        stream.pipe(res);
-      });
-    }
-  });
+        return stream.pipe(res);
+      } else {
+        const stream = await song.downloadProgressive();
+        const writer = stream.pipe(fs.createWriteStream(isfile));
+        writer.on("finish", () => {
+          const stream = fs.createReadStream(isfile);
+          const data = fs.readFileSync(isfile);
+
+          res.setHeader("content-type", "audio/mpeg");
+          res.setHeader("Accept-Ranges", "bytes");
+          res.setHeader("content-length", data.length);
+          res.setHeader(
+            "Content-Disposition",
+            `attachment; filename="${File}.mp3"`
+          );
+          stream.pipe(res);
+        });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
