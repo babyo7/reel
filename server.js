98bfcf7bb071cac0d94994a9366b4f9629a613fa
babyo77 <onlybabyyo7@gmail.com>
2024-04-14 19:11:28 +0530

    new

--- server.js
+++ server.js
@@ -2,25 +2,34 @@ const app = require("express")();
 const cors = require("cors");
 const stream = require("./stream");
 const fileStream = require("./fileStream");
+const cluster = require("node:cluster");
+const os = require("os");
+const totalCpu = os.cpus().length;
 
-const PORT = process.env.PORT || 4000;
-app.use(cors());
-
-app.get("/", (req, res) => {
-  const url = req.query.url;
-  if (!url) return res.status(404).json("url not provided");
-  const download = req.query.file;
-  try {
-    if (download) {
-      fileStream(res, url, download);
-    } else {
-      stream(res, url);
-    }
-  } catch (error) {
-    res.status(500).json(error.message);
+if (cluster.isPrimary) {
+  for (let i = 0; i < totalCpu; i++) {
+    cluster.fork();
   }
-});
+} else {
+  const PORT = process.env.PORT || 4000;
+  app.use(cors());
 
-app.listen(PORT, () => {
-  console.log(`http://localhost:${PORT}`);
-});
+  app.get("/", (req, res) => {
+    const url = req.query.url;
+    if (!url) return res.status(404).json("url not provided");
+    const download = req.query.file;
+    try {
+      if (download) {
+        fileStream(res, url, download);
+      } else {
+        stream(res, url);
+      }
+    } catch (error) {
+      res.status(500).json(error.message);
+    }
+  });
+
+  app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+  });
+}
