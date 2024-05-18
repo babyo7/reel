23aa1a377d592be1afc859e804bdc8c2d7ef14da
Babyo7_ <156928258+babyo777@users.noreply.github.com>
2024-05-18 23:15:54 +0530

    Update stream.js

--- stream.js
+++ stream.js
@@ -8,56 +8,32 @@ module.exports = function stream(res, url, req) {
     .then(async (song) => {
       const isfile = `music/${song.id}.mp3`;
       if (fs.existsSync(isfile)) {
+         const stream = fs.createReadStream(isfile);
         const data = fs.readFileSync(isfile);
-        const range = req.headers.range;
-        const totalSize = data.length;
-        if (range) {
-          const parts = range.replace(/bytes=/, "").split("-");
-          const start = parseInt(parts[0], 10);
-          const end = parts[1] ? parseInt(parts[1], 10) : totalSize - 1;
-
-          const chunkSize = end - start + 1;
-          const stream = fs.createReadStream(isfile, {
-            start,
-            end,
-          });
-          stream.pipe(res);
-          res.writeHead(206, {
-            "Content-Range": `bytes ${start}-${end}/${totalSize}`,
-            "Content-Length": chunkSize,
-            "Content-Type": "audio/mpeg",
-            "Accept-Ranges": "bytes",
-          });
-        } else {
-          res.status(403).send();
-        }
+        res.setHeader("content-type", "audio/mpeg");
+        res.setHeader("Accept-Ranges", "bytes");
+        res.setHeader("content-length", data.length);
+        res.setHeader(
+          "Content-Disposition",
+          `attachment; filename="${File}.mp3"`
+        );
+        return stream.pipe(res);
+       
       } else {
         const stream = await scdl.download(url);
         const writer = stream.pipe(fs.createWriteStream(isfile));
         writer.on("finish", () => {
-          const data = fs.readFileSync(isfile);
-          const range = req.headers.range;
-          const totalSize = data.length;
-          if (range) {
-            const parts = range.replace(/bytes=/, "").split("-");
-            const start = parseInt(parts[0], 10);
-            const end = parts[1] ? parseInt(parts[1], 10) : totalSize - 1;
-
-            const chunkSize = end - start + 1;
-            const stream = fs.createReadStream(isfile, {
-              start,
-              end,
-            });
-            stream.pipe(res);
-            res.writeHead(206, {
-              "Content-Range": `bytes ${start}-${end}/${totalSize}`,
-              "Content-Length": chunkSize,
-              "Content-Type": "audio/mpeg",
-              "Accept-Ranges": "bytes",
-            });
-          } else {
-            res.status(403).send();
-          }
+            const stream = fs.createReadStream(isfile);
+        const data = fs.readFileSync(isfile);
+        res.setHeader("content-type", "audio/mpeg");
+        res.setHeader("Accept-Ranges", "bytes");
+        res.setHeader("content-length", data.length);
+        res.setHeader(
+          "Content-Disposition",
+          `attachment; filename="${File}.mp3"`
+        );
+        return stream.pipe(res);
+       
         });
       }
     })
